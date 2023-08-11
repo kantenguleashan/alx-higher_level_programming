8359605cb97128aad65cb03cb4da196372518f2b
kantenguleashan <kantenguleshan@gmail.com>
2023-08-11 14:18:42 -0700

    first file

---
+++ x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#import from add function file
+from add_0 import add
+
+def main():
+    a = 1
+    b = 2
+    result=add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+if__name__ == "__main__":
+    main()
