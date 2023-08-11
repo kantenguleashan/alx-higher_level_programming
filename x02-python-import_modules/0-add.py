145f188bc129f0c1ce90d83b34865c31ebf8adcb
kantenguleashan <kantenguleshan@gmail.com>
2023-08-11 14:27:01 -0700

    push

--- x02-python-import_modules/0-add.py
+++ x02-python-import_modules/0-add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-#import from add function file
-from add_0 import add
-
-def main():
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    result=add(a, b)
-    print("{} + {} = {}".format(a, b, result))
-
-if__name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
