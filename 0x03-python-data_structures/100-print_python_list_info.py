f089242da115ea8f20ab933b80c1c4d9b69b4e8e
kantenguleashan <kantenguleshan@gmail.com>
2023-08-14 14:29:20 -0700

    data

---
+++ 0x03-python-data_structures/100-print_python_list_info.py
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints python list info
+ *
+ * @p: PyObject
+ * Return: no return
+ */
+void print_python_list_info(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *item;
+
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+
