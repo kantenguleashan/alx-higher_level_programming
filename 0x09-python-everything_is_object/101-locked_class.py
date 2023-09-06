d0e843b633221d9cdce73e2fbdc26fc3d9b5a3be
kantenguleashan <kantenguleshan@gmail.com>
2023-09-06 05:28:21 -0700

    py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""LOCKED CLASS DEFINITION"""
+
+
+class LockedClass:
+    """
+   PREVENT USER FORM MAKING NEW INSTATNCES
+  FOR LOCKED CLASS ATTRIBUTES'.
+    """
+
+    __slots__ = ["first_name"]
+
